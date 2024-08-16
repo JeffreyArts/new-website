18a7340dd9409da9ee9342ee28e29c6b7da1d1d7
Jeffrey Arts <sjeffff@gmail.com>
2024-08-16 16:25:39 +0200

    Mobile friendly header menu

---
+++ src/components/jao-icon.vue
@@ -0,0 +1,248 @@
+<template>
+    <svg class="jao-icon" version="1.1" :viewBox="`0 0 ${svgWidth} ${svgHeight}`" xmlns="http://www.w3.org/2000/svg">
+        <g v-if="displayGrid.length > 0">
+            <rect class="jao-icon-cell" v-for="(cell,x) in displayGrid" :key="x"
+                :x="1 + cell.x * 10" 
+                :y="1+ cell.y * 10" 
+                :v="cell.v" 
+                width="8" 
+                height="8"
+                :style="`fill:${cell.color};`"
+                />
+        </g>
+    </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue"
+import _ from "lodash"
+import gsap from "gsap"
+import { iconsMap } from "jao-icons"
+
+/* 
+This icon component uses a two-dimensional grid, with cells of 8x8 pixels - with a gap of 1px around each cell
+
+You'll use it by specifing a size (small, medium or large), in combination with the name of the icon you'd like to display
+You can also specify entire custom icons by specifying a two-dimensional array with truthy/falsey values
+
+small = 9x9
+medium = 13x13
+large = 21x21
+
+*/
+
+interface CustomGridPoint {
+    x: number;
+    y: number;
+    value: 0 | 1;
+}
+interface transitEffect {
+    duration?: number;
+    ease?: string;
+    delay?: number,
+    effect?: "left-to-right" | "right-to-left" | "top-to-bottom" | "bottom-to-top" | "shuffle"
+}
+
+export default defineComponent ({
+    name: "SiteMenu", 
+    props: {
+        size: {
+            type: String  as PropType<"small" | "medium" | "large">,
+            required: true
+        },
+        inactiveColor: {
+            type: String,
+            required: false
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        custom: {
+            type: Array as PropType<CustomGridPoint[]>,
+            required: false
+        },
+        transitEffect: {
+            type: Object as PropType<transitEffect>,
+            required: false
+        }
+    },
+    data() {
+        return {
+            iconsMap: iconsMap,
+            originalIcon: [] as Array<Array<0 | 1>>,
+            icon: [] as Array<Array<0 | 1>>,
+            originalGrid: [] as Array<{ x:number, y:number, v:1|0, color: string }>,
+            displayGrid: [] as Array<{ x:number, y:number, v:1|0, color: string }>,
+            transitions: [] as Array<gsap.core.Tween>,
+        }
+    },
+    computed: {
+        svgWidth() {
+            if (this.icon.length <= 0) {
+                return 0
+            }
+            return this.icon.length * 10
+        },
+        svgHeight() {
+            if (this.icon.length <= 0) {
+                return 0
+            }
+            return this.icon[0].length * 10
+        },
+    },
+    watch:{
+        "size":{
+            handler: function () {
+                this.updateIcon()
+            },
+            immediate: true
+        },
+        "name":{
+            handler: function () {
+                this.updateIcon()
+            },
+            immediate: true
+        },
+        "custom":{
+            handler: function () {
+                if (!this.$el) {
+                    return
+                }
+                if (this.custom && this.custom.length > 0) {
+                    this.originalGrid = []
+                    const activeColor = window.getComputedStyle(this.$el).color
+                    const inactiveColor = this.inactiveColor ? this.inactiveColor : "#efefef"
+                    _.each(this.custom, (val) => {
+                        const data = {
+                            x: val.x,
+                            y: val.y,
+                            v: val.value,
+                            color: val.value ? activeColor : inactiveColor
+                        }
+                        this.originalGrid.push(data)
+                    })
+                    this.transit({ duration: 0, delay: 0 })
+                }
+            },
+            deep: true,
+            immediate: true
+        },
+    },
+    mounted() {
+        this.updateIcon()
+    },
+    unmounted() {
+        //
+    },
+    methods: {
+        updateIcon() {
+            
+            
+            if (!this.$el) {
+                return
+            }
+
+            if (this.size && this.iconsMap[this.size] && typeof this.name === "string") {
+                this.icon = this.iconsMap[this.size][this.name]
+            }
+
+            if (!this.icon) {
+                throw new Error("Invalid icon")
+            }
+            
+            this.originalGrid = []
+            const activeColor = window.getComputedStyle(this.$el).color
+            const inactiveColor = this.inactiveColor ? this.inactiveColor : "#efefef"
+            
+            if (this.icon.length <= 0 && this.custom) {
+                this.customGridToIcon()
+            }
+
+            _.each(this.icon, (row,y) => {
+                _.each(row, (val,x) => {
+                    const data = {
+                        x,
+                        y,
+                        v: val,
+                        color: val ? activeColor : inactiveColor
+                    }
+                    this.originalGrid.push(data)
+                })
+            })
+
+
+            if (this.displayGrid.length !== this.originalGrid.length) {
+                this.displayGrid = []
+                _.each(this.originalGrid,(grid) => {
+                    this.displayGrid.push({ 
+                        x:grid.x,
+                        y:grid.y,
+                        v:grid.v,
+                        color: grid.color 
+                    })
+                })
+            }
+                    
+            this.transit(this.transitEffect)
+        },
+        customGridToIcon() {
+            if (!this.custom) {
+                console.warn("No custom grid to be transform to icon")
+                return []
+            }
+            _.each(_.sortBy(this.custom, ["y", "x"]), point => {
+                if (!this.icon[point.y]) {
+                    this.icon.push([])
+                }
+                this.icon[point.y].push(!!point.value)
+            })
+        },
+        transit(opts = {
+            duration: .4,
+            delay: 0,
+            ease: "",
+            effect: "" as "left-to-right" | "right-to-left" | "top-to-bottom" | "bottom-to-top" | "shuffle"
+        } as transitEffect) {
+
+            if (this.transitions.length > 0) {
+                _.each(this.transitions, t => {t.kill()})
+                this.transitions = []
+            }
+
+            let effect = _.isString(opts.effect) ? opts.effect : "fade-in"
+            let ease = _.isString(opts.ease) ? opts.ease : "linear"
+            let duration = _.isNumber(opts.duration) ? opts.duration : .4
+            let delay = _.isNumber(opts.delay) ? opts.delay : 0
+
+            let collection = this.originalGrid
+            if (effect == "shuffle") {
+                collection = _.shuffle(this.originalGrid)
+            } else if (effect == "top-to-bottom") {
+                collection = this.originalGrid
+            } else if (effect == "bottom-to-top") {
+                collection = _.reverse(this.originalGrid)
+            } else if (effect == "left-to-right") {
+                collection = _.sortBy(this.originalGrid, "x")
+            } else if (effect == "right-to-left") {
+                collection = _.reverse(_.sortBy(this.originalGrid, "x"))
+            } 
+
+            _.each(collection, (grid, index) => {
+                const cell = _.find(this.displayGrid, { x:grid.x, y:grid.y })
+                if (cell) {
+                    this.transitions.push(gsap.to(cell, {
+                        color: grid.color,
+                        ease: ease,
+                        delay: Number(delay) * index,
+                        duration
+                    }))
+                }
+            })
+        
+                
+        }
+    }
+})
+
+</script>
--- src/components/site-header.vue
+++ src/components/site-header.vue
@@ -4,18 +4,17 @@
             <span class="site-header-logo" ref="logo"></span>
         </RouterLink>
         <nav class="site-header-navigation">
-            <span class="site-header-navigation-item" v-for="navItem, key in nav" :key="key" @click="expand($event, navItem)" @mouseenter="expand($event, navItem)" @mouseleave="closeExpandedItem($event, navItem)">
+            <span class="site-header-navigation-item" v-for="navItem, key in nav" :key="key">
                 
-                <RouterLink :to="navItem.link" v-if="navItem.subitems.length === 0 || (navItem.subitems.length > 0 && navItem.expanded)">
+                <RouterLink :to="navItem.link">
                     {{ navItem.name }}
                 </RouterLink>
-
-                <span v-if="navItem.subitems.length > 0 && !navItem.expanded">
-                    {{ navItem.name }}
-                </span>
-
-                <div class="site-header-navigation-dropdown" v-if="navItem.subitems" :id="`item${navItem.id}`">
-                    <RouterLink class="site-header-navigation-dropdown-item" :to="subItem.link" v-for="subItem, subKey in navItem.subitems" :key="subKey" @click="navItem.expanded = false">{{subItem.name}}</RouterLink>
+                
+                <div class="site-header-navigation-dropdown" v-if="navItem.subitems" :id="`item${navItem.id}`" >
+                    <RouterLink class="site-header-navigation-dropdown-item" :to="subItem.link" v-for="subItem, subKey in navItem.subitems" :key="subKey" @click="closeMenu">
+                        <jaoIcon size="small" name="play" />
+                        {{subItem.name}}
+                    </RouterLink>
                 </div>
             </span>
         </nav>
@@ -27,7 +26,7 @@
 import { defineComponent } from "vue"
 import { Icon } from "jao-icons"
 import Navigation from "@/services/payload/navigation"
-import _ from "lodash"
+import jaoIcon from "@/components/jao-icon.vue"
 import gsap from "gsap"
 
 type NavItem = {
@@ -45,6 +44,7 @@ type NavItem = {
 export default defineComponent({
     name: "languageSelector",
     components: {
+        jaoIcon
     },
     data: () => {
         return {
@@ -62,7 +62,6 @@ export default defineComponent({
         }) 
     },
     mounted() {
-        console.log(this.nav)
         const logo = this.$refs["logo"] as HTMLElement
         const icon = Icon("medium/logo")
         // this.loadMenu()
@@ -71,6 +70,20 @@ export default defineComponent({
         }
     },
     methods: {
+        closeMenu(e: Event) {
+            const targetEl = e.target as HTMLElement
+            if (!targetEl) {
+                return
+            }
+            const parentEl = targetEl.parentElement
+            if (!parentEl) {
+                return
+            }
+            parentEl.style.display = "none"
+            setTimeout(()=> {
+                parentEl.style.display = ""
+            },110)
+        },
         closeExpandedItem(e: Event, navItem: NavItem) {
             if (!navItem) {
                 return
@@ -228,24 +241,58 @@ export default defineComponent({
         text-decoration: none;
         color: var(--contrast-color);
     }
+
+    &:hover,
+    &:focus {
+        .site-header-navigation-dropdown {
+            display: flex;
+
+            .site-header-navigation-dropdown-item {
+                opacity: 1;
+                pointer-events: all;
+            }
+        }
+    }
 }
 
 .site-header-navigation-dropdown {
     position: absolute;
     top: calc(100% - 4px);
-    display: flex;
+    display: none;
     flex-flow: column;
     width: 128px;
     
 }
 
 .site-header-navigation-dropdown-item {
-    display: inline-block;
+    display: flex;
     padding: 4px 0;
     font-size: .8em;
     opacity: 0;
     pointer-events: none;
+    transition: $transitionDefault;
+    transition-delay: .2s;
+    align-items: center;
+
+    &:hover {
+        .jao-icon {
+            opacity: 1;
+        }
+    }
 }
+
+.site-header-navigation-dropdown-item .jao-icon {
+    height: 8px;
+    padding-right: 4px;
+    margin-left: -12px;
+    opacity: 0;
+    
+    [v="0"] {
+        fill: transparent !important;
+    }
+}
+
+
 
 @media all and (min-width: 640px) {
     .site-header-logo,
@@ -264,6 +311,12 @@ export default defineComponent({
 
     .site-header-navigation-dropdown {
         top: calc(100%);
+    }
+    
+    .site-header-navigation-dropdown-item .jao-icon {
+        height: 20px;
+        padding-right: 8px;
+        margin-left: -28px;
     }
 }
 
