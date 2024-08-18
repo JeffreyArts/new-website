1e229618d23878b79df78986b1a83d0fc4cb3d0e
Jeffrey Arts <sjeffff@gmail.com>
2024-08-18 15:14:53 +0200

    simplify code

--- src/components/slate-text.vue
+++ src/components/slate-text.vue
@@ -1,6 +1,5 @@
 <template>
-    <!-- <component :is="compiledTemplate"></component> -->
-     <div></div>
+    <component :is="compiledTemplate"></component>
 </template>
   
 <script lang="ts">
@@ -20,6 +19,62 @@ export interface SlateNode {
     url?: string
 }
 
+const serialize = (node: SlateNode) => {
+    let text = ""
+    if (Array.isArray(node)) {
+        text = node.map(n => serialize(n)).join("")
+    }
+    if (Array.isArray(node.children)) {
+        text = node.children.map(n => serialize(n)).join("")
+    }
+
+    if (node.text) {
+        text += node.text
+    }
+
+    if (node.type === "ol") { text = `<ol>${text}</ol>` }
+    if (node.type === "ul") { text = `<ul>${text}</ul>` }
+    if (node.type === "li") { text = `<li>${text}</li>` }
+    if (node.type === "h1") { text = `<h1>${text}</h1>` }
+    if (node.type === "h2") { text = `<h2>${text}</h2>` }
+    if (node.type === "h3") { text = `<h3>${text}</h3>` }
+    if (node.type === "h4") { text = `<h4>${text}</h4>` }
+    if (node.type === "h5") { text = `<h5>${text}</h4>` }
+    if (node.type === "h6") { text = `<h6>${text}</h4>` }
+
+    if (node.bold) {
+        text = `<strong>${text}</strong>`
+    }
+
+    if (node.italic) {
+        text = `<em>${text}</em>`
+    }
+
+    if (node.strikethrough) {
+        text = `<s>${text}</s>`
+    }
+
+    if (node.underline) {
+        text = `<u>${text}</u>`
+    }
+
+    if (node.type === "link" && node.url) {
+        if (node.url[0] != "/") {
+            const s = new XMLSerializer()
+            const svg = Icon("medium/external-link")
+            if (svg) {
+                text = `<a href="${node.url}">${text}${s.serializeToString(svg)}</a>`
+            } else {
+                text = `<a href="${node.url}">${text}🔗</a>`
+            }
+                    
+        } else {
+            text = `<RouterLink to="${node.url}">${text}</RouterLink>`
+        }
+    }
+    return text
+}
+
 export default defineComponent({
     props: {
         data: {
@@ -28,65 +83,6 @@ export default defineComponent({
         }
     },
     setup(props) {
-        const serialize = (node: SlateNode) => {
-            let text = ""
-            console.log(node)
-            if (Array.isArray(node)) {
-                text = node.map(n => serialize(n)).join("")
-            }
-            if (Array.isArray(node.children)) {
-                text = node.children.map(n => serialize(n)).join("")
-            }
-
-            if (node.text) {
-                text += node.text
-            }
-
-            if (node.type === "ol") { text = `<ol>${text}</ol>` }
-            if (node.type === "ul") { text = `<ul>${text}</ul>` }
-            if (node.type === "li") { text = `<li>${text}</li>` }
-            if (node.type === "h1") { text = `<h1>${text}</h1>` }
-            if (node.type === "h2") { text = `<h2>${text}</h2>` }
-            if (node.type === "h3") { text = `<h3>${text}</h3>` }
-            if (node.type === "h4") { text = `<h4>${text}</h4>` }
-            if (node.type === "h5") { text = `<h5>${text}</h4>` }
-            if (node.type === "h6") { text = `<h6>${text}</h4>` }
-
-            if (node.bold) {
-                text = `<strong>${text}</strong>`
-            }
-
-            if (node.italic) {
-                text = `<em>${text}</em>`
-            }
-
-            if (node.strikethrough) {
-                text = `<s>${text}</s>`
-            }
-
-            if (node.underline) {
-                text = `<u>${text}</u>`
-            }
-
-            if (node.type === "link" && node.url) {
-                if (node.url[0] != "/") {
-                    const s = new XMLSerializer()
-                    const svg = Icon("medium/external-link")
-                    if (svg) {
-                        text = `<a href="${node.url}">${text}${s.serializeToString(svg)}</a>`
-                    } else {
-                        text = `<a href="${node.url}">${text}🔗</a>`
-                    }
-                    
-                } else {
-                    text = `<RouterLink to="${node.url}">${text}</RouterLink>`
-                }
-            }
-            return text
-        }
-        
-        // console.log()
-        
         // Compile the HTML string into a render function
         const compiledTemplate = computed(() => compile(serialize(props.data)))
   
