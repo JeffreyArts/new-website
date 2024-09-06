04c4bd18148b0c942995280a2664e794b1d5804d
Jeffrey Arts <sjeffff@gmail.com>
2024-09-06 16:58:36 +0200

    Add polyfill for aspect-ratio

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
+import postcssAspectRatioPolyfill from 'postcss-aspect-ratio-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    ssgOptions: {
-        script: "async",
+    css: {
+      postcss: {
+        plugins: [
+          postcssAspectRatioPolyfill(), // Add the polyfill plugin here
+        ],
+      },
     },
+    plugins: [vue()],
     resolve: {
         "alias": [
             { find: "esmBundler", replacement: "vue/dist/vue.esm-bundler.js" },
