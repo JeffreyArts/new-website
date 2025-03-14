5d9821f5e12a72ce638e679465eaa43ed2369fca
Jeffrey Arts <sjeffff@gmail.com>
2025-03-14 18:24:53 +0100

    Add source for subscriber to be able to manage subscribers from multiple sources

--- src/services/payload/newsletter-subscription.ts
+++ src/services/payload/newsletter-subscription.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 const payloadNewsletterSubscription = {
     collectionName: "newsletter-subscription",
-    add: (email: string) => axios.post(`${import.meta.env.VITE_PAYLOAD_REST_ENDPOINT}/newsletter-subscriptions`, { email })
+    add: (email: string) => axios.post(`${import.meta.env.VITE_PAYLOAD_REST_ENDPOINT}/newsletter-subscriptions`, { email, source: "Website" })
 }
 
 export default payloadNewsletterSubscription
